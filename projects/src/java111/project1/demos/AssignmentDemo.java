6ecd62ce8a28c5aac332004718897cc0a60aff75
Paula Waite <pawaite@madisoncollege.edu>
2018-06-13 15:49:26 -0500

    AssignmentDemo added

---
+++ projects/src/java111/project1/demos/AssignmentDemo.java
@@ -0,0 +1,18 @@
+/** Demonstrate assigning values to variables and displaying them
+ *  @author Paula Waite
+ */
+ public class AssignmentDemo {
+     /** Create an int variable and a String variable, assign
+      *  values to them, and then display.
+      */
+      public static void main(String[] args) {
+          int myNumber = 500.500;
+          String myString = "Hello Everyone!";
+
+          System.out.println("myNumber is " + myNumber);
+          System.out.println("myString is " + myString);
+
+      }
+
+
+ }
