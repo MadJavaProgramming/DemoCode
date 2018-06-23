b807a362689a020d79f9652d26f8338f1d0b3227
Paula Waite <pawaite@madisoncollege.edu>
2018-06-23 14:05:38 -0500

    after vehicle debugging demo

--- projects/src/java111/project1/demos/VehicleTestDrive.java
+++ projects/src/java111/project1/demos/VehicleTestDrive.java
@@ -12,17 +12,27 @@
       */
       public static void main(String[] args) {
 
-          // instantiate a vehicle object, and assigning it to the vehicle 1 variable
+          // instantiate a vehicle object, and assigning it to the vehicle1 variable
           Vehicle vehicle1 = new Vehicle();
 
-          // set some details on the objects
+          // instantiate a second vehicle object, and assigning it to the vehicle2 variable
+          Vehicle vehicle2 = new Vehicle();
+
+          // set some details on the first vehicle object
           vehicle1.make = "Chevy";
           vehicle1.model = "Blazer";
           vehicle1.year = 2003;
           vehicle1.color = "grey";
 
-          // call the start method on vehicle1
+          // set some details on the second vehicle object
+          vehicle2.make = "Toyota";
+          vehicle2.model = "4-Runner";
+          vehicle2.year = 2012;
+          vehicle2.color = "black";
+
+          // call the start method on vehicle1 and vehicle 2
           vehicle1.start();
+          vehicle2.start();
       }
 
 
