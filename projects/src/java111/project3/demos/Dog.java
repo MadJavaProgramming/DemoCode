6f84bc75b42e95a427c2a5f63534e3e6894ac141
Paula Waite <pawaite@madisoncollege.edu>
2018-07-05 17:09:47 -0500

    adding full kennel application

--- projects/src/java111/project3/demos/Dog.java
+++ projects/src/java111/project3/demos/Dog.java
@@ -1,26 +1,90 @@
-/** Represents a dog in our kennel application
- * @author pwaite
+/**
+ *  This class holds information about an individual dog
+ *
+ * @author    eknapp
  */
- public class Dog {
+public class Dog {
 
-     private int weight;
+    private  String  name;
+    private  String  owner;
+    private  int     weight;
 
-     /** set the dog's weight
-      * @param newWeight dog's weight in pounds
-      */
-      public void setWeight(int newWeight) {
-          weight = newWeight;
+    /**
+     *  Sets the name attribute of the Dog object
+     *
+     * @param  name  The new name value
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-      }
-      /** determine the food needed for single day for the dog based on weight
-       *  @return amount of food needed
-       */
-       public int calculateFoodNeeded() {
-           if (weight < 50) {
-               return 1;
-           } else {
-               return 2;
-           }
+    /**
+     *  Gets the name attribute of the Dog object
+     *
+     * @return    The name value
+     */
+    public String getName() {
+        return name;
+    }
 
-       }
- }
+    /**
+     *  Sets the owner attribute of the Dog object
+     *
+     * @param  owner  The new owner value
+     */
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    /**
+     *  Gets the owner attribute of the Dog object
+     *
+     * @return    The owner value
+     */
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     *  Sets the weight attribute of the Dog object
+     *
+     * @param  weight  The new weight value
+     */
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    /**
+     *  Gets the weight attribute of the Dog object
+     *
+     * @return    The weight value
+     */
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     *  Description of the Method
+     *
+     * @return    Description of the Return Value
+     */
+    public int calculateFoodNeeded() {
+        if (weight <= 50) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    /**
+     *  Description of the Method
+     *
+     * @return    Description of the Return Value
+     */
+    public String display() {
+        return "Name: " + getName()
+                + "\nOwner: " + getOwner()
+                + "\nWeight: " + getWeight()
+                + "\nFood Needed: " + calculateFoodNeeded();
+    }
+}
