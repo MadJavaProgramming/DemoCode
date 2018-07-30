f015092a28cd98d026bd2b0f85d1901a50e171a0
Paula Waite <pawaite@madisoncollege.edu>
2018-07-30 12:21:20 -0500

    fixing conflicts

--- projects/src/java111/project5/LabOneFirstClass.java
+++ projects/src/java111/project5/LabOneFirstClass.java
@@ -11,11 +11,7 @@ public class LabOneFirstClass {
      * This is the main method for this class which will
      * output a welcome message.
      *
-<<<<<<< HEAD
      * @param args command line arguments
-=======
-     * @param args
->>>>>>> a9d73eaca3662a82bbd08ffe2441d17ac4836ba1
      */
     public static void main(String[] args) {
 
